19c88878972e26736a0f477a46ffaa59ba7e352e
kavispires <kavispires@gmail.com>
2016-11-18 16:37:47 -0800

    feat: BG.notNumber function

--- js/bg-0.0.1.js
+++ js/bg-0.0.1.js
@@ -3,6 +3,23 @@
     function define_BG(){
         var BG = {};
 
+        /* ---------
+           UTILITY
+           --------- */
+
+        /** 
+        * @description Converts the value to a number, then checks if it is a number.
+        * @param {anything} val
+        * @returns {number} The number as a interger or as 0
+        */
+        BG.notNumber = function(val){
+            val = Number(val);
+            if (isNaN(val)) {
+                return 0;
+            }
+            return val;
+        };
+
         return BG;
     }
     //define globally if it doesn't already exist
